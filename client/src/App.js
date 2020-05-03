816eb2076fdaf4a350cef2022e36e79da9c356fd
Michael Wells <mwells1286@gmail.com>
2020-05-03 16:32:16 -0700

    add post functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import PostCreate from './PostCreate'
+import PostList from './PostList';
 export default () => {
-    return <div className="container">
-        <h1>Blog App</h1>
-        <PostCreate />
-    </div>
+    return (
+        <div className="container">
+            <h1>Blog App</h1>
+            <PostCreate />
+            <hr />
+            <h1>Posts</h1>
+            <PostList />
+        </div>
+    );
 }
