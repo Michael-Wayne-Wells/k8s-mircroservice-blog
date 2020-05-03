8769791af4850b8704978e5f53b72cbd339e8bd9
Michael Wells <mwells1286@gmail.com>
2020-05-03 11:48:57 -0700

    update post to posts.

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -9,15 +10,16 @@ const posts = {};
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
+
 app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
-  post[id] = {
+  posts[id] = {
     id, title
   };
 
-  res.status(201).send(post[id]);
+  res.status(201).send(posts[id]);
 });
 app.listen(4000, () => {
   console.log('Listening on 4000');
